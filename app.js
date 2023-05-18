a6019224139ee234e8b4da9b6f34809b278eccb4
houstontu0 <90640953+houstontu0@users.noreply.github.com>
2023-05-18 15:45:15 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,54 @@
+//GENERATE A SECRET API KEY FROM https://platform.openai.com/account/api-keys
+//PASTE YOUR SECRET API KEY INTO LINE 3.
+
+const API_KEY = '**PASTE YOUR OPENAI API KEY HERE**'
+const submitButton = document.querySelector('#submit')
+const outPutElement = document.querySelector('#output')
+const inputElement = document.querySelector('input')
+const historyElement = document.querySelector('.history')
+const buttonElement = document.querySelector('button')
+
+function changeInput(value) {
+    const inputElement = document.querySelector('input')
+    inputElement.value = value
+}
+
+async function getMessage() {
+    console.log('clicked')
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify ( {
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: inputElement.value}],
+            max_tokens: 100
+        })
+    }
+    try {
+        const response = await fetch ('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        console.log(data)
+        outPutElement.textContent = data.choices[0].message.content
+        if (data.choices[0].message.content && inputElement.value){
+            const pElement = document.createElement('p')
+            pElement.textContent = inputElement.value
+            pElement.addEventListener('click', () => changeInput(pElement.textContent))
+            historyElement.append(pElement)
+        }
+    } catch (error) {
+        console.error(error)
+
+    }
+
+}
+
+submitButton.addEventListener('click', getMessage)
+
+function clearInput () {
+    inputElement.value = ''
+}
+
+buttonElement.addEventListener('click', clearInput)
